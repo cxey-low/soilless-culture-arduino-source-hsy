2d4f0028e5b41db5126dbe23265340e2a2275614
CXeY <1624202438@qq.com>
2023-11-07 13:46:17 +0800

    tryed write and createChar

--- soilless_culture.ino
+++ soilless_culture.ino
@@ -11,21 +11,43 @@ I2C device found at address 0x7C  !
 
 LiquidCrystal_I2C lcd(0x20, 16, 2);
 
+byte cus[8] = {
+    0b00000,
+    0b01010,
+    0b11111,
+    0b11111,
+    0b01110,
+    0b00100,
+    0b00000,
+    0b00000};
+
+byte ctem[8] = {
+    0b11100,
+    0b10111,
+    0b11100,
+    0b01100,
+    0b01100,
+    0b01111,
+    0b00111,
+    0b00111};
+
 void clean()
 {
     lcd.home();
     lcd.print("              ");
-    lcd.setCursor(0,1);
+    lcd.setCursor(0, 1);
     lcd.print("              ");
 }
 
 void hello()
 {
-    //clean();
+    // clean();
     lcd.setCursor(0, 0);
     lcd.print("  Hello world!  ");
     lcd.setCursor(0, 1);
-    lcd.print("      CXeY      ");
+    lcd.write(0);
+    lcd.setCursor(1, 1);
+    lcd.print("     CXeY      ");
 }
 
 void setup()
@@ -35,6 +57,9 @@ void setup()
     lcd.backlight();
     lcd.home();
     lcd.clear();
+
+    lcd.createChar(0, cus);
+    lcd.createChar(1,ctem);
 
     hello();
 
@@ -55,16 +80,18 @@ void loop()
 
     if (digitalRead(tem_button))
     {
-        //clean();
+        // clean();
         lcd.home();
         lcd.print("Tem:              ");
         lcd.setCursor(5, 1);
         lcd.print("          ");
-        lcd.setCursor(0,1);
+        lcd.setCursor(0, 1);
         lcd.print((double)(analogRead(tem_sensor) * (5 / 10.24)));
-    }else if (!(digitalRead(tem_button)))
+        lcd.setCursor(6,1);
+        lcd.write(1);
+    }
+    else if (!(digitalRead(tem_button)))
     {
         hello();
     }
-    
 }
