47c10912623250eb9cb519772c7ea9a02f1b0f30
CXeY <1624202438@qq.com>
2023-11-13 13:25:08 +0800

    new system

--- soilless_culture.ino
+++ soilless_culture.ino
@@ -1,8 +1,7 @@
 #include <LiquidCrystal_I2C.h>
 
 #define ROTATION A15
-#define anaRO map(analogRead(A15),0,1023,1,4)    
-
+#define anaRO map(analogRead(A15), 0, 1023, 1, 4)
 
 #define STEAM A10
 #define TEM A9
@@ -10,14 +9,13 @@
 
 #define WAIT 1000
 
-
 LiquidCrystal_I2C lcd(0x20, 16, 2);
 
 void hello()
 {
     // clean();
     lcd.setCursor(0, 0);
-    lcd.print("  Hello world!  ");
+    lcd.print("  Hellooworld!  ");
     lcd.setCursor(0, 1);
     lcd.setCursor(1, 1);
     lcd.print("     CXeY      ");
@@ -48,7 +46,7 @@ void loop()
 {
     if (Serial.available())
     {
-        switch ((char) Serial.read())
+        switch ((char)Serial.read())
         {
         case 'd':
 
@@ -58,7 +56,7 @@ void loop()
             Serial.println(analogRead(TEM));
             Serial.println("LIGHT");
             Serial.println(analogRead(LIGHT));
-            Serial.println("STEAM");
+            Serial.println("STEAM1");
             Serial.println(analogRead(STEAM));
 
             break;
@@ -72,37 +70,34 @@ void loop()
     case 1:
         lcd.home();
         lcd.print("STEAM         ");
-        lcd.setCursor(0,1);
+        lcd.setCursor(0, 1);
         lcd.print("           ");
-        lcd.setCursor(0,1);
+        lcd.setCursor(0, 1);
         lcd.print(analogRead(STEAM));
-        lcd.setCursor(5,1);
+        lcd.setCursor(5, 1);
         delay(WAIT);
         break;
     case 2:
         lcd.home();
         lcd.print("Tem           ");
-        lcd.setCursor(0,1);
+        lcd.setCursor(0, 1);
         lcd.print("           ");
-        lcd.setCursor(0,1);
-        lcd.print((double) analogRead(TEM)*(5/10.24));
-        lcd.setCursor(6,1);
+        lcd.setCursor(0, 1);
+        lcd.print((double)analogRead(TEM) * (5 / 10.24));
+        lcd.setCursor(6, 1);
         delay(WAIT);
         break;
     case 3:
         lcd.home();
         lcd.print("LIGHT         ");
-        lcd.setCursor(0,1);
+        lcd.setCursor(0, 1);
         lcd.print("           ");
-        lcd.setCursor(0,1);
+        lcd.setCursor(0, 1);
         lcd.print(analogRead(LIGHT));
-        lcd.setCursor(5,1);
+        lcd.setCursor(5, 1);
         delay(WAIT);
         break;
     default:
         break;
     }
-
-
-
 }
